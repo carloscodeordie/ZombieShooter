ea78846ec07919386af4b4ab76d95627a7edb06f
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-11-17 14:19:37 -0600

    Add a raycast to weapon and check which gameobject was hitted
    Add a raycast to weapon and check which gameobject was hitted

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamara;
+    [SerializeField] float range = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessShooting();
+    }
+
+    private void ProcessShooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamara.transform.position, FPCamara.transform.forward, out hit, range))
+        {
+            Debug.Log("The raycast hit the following: " + hit.transform.name);
+        }
+        
+    }
+}
