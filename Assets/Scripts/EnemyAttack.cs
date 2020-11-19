d433f9d71701c5309a4d53cb57837a7f0d82c5c6
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-11-19 16:15:33 -0600

    Add player health count in the game
    Add player health count in the game

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] PlayerHealth target;
     [SerializeField] float damage = 40f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerHealth>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) { return; }
-        Debug.Log("Bang Bang");
+        target.TakeDamage(damage);
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log("Player was hitted, player's health is: " + health);
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log("Player is death");
+    }
+}
