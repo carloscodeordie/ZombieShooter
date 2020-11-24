65f8a48620eb371752b2e552611de063e165ab6a
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-11-24 09:30:20 -0600

    Add zoom feature to weapon
    Add zoom feature to weapon

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedInSensibility = 0.5f;
+    [SerializeField] float zoomedOutSensibility = 2f;
+
+    RigidbodyFirstPersonController fpsController;
+
+    bool zoomToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+        zoomedOutSensibility = fpsController.mouseLook.XSensitivity;
+    }
+
+    private void Update()
+    {
+        HandleZoom();
+    }
+
+    private void HandleZoom()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomToggle)
+            {
+                zoomToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+            adjustMouseSensibility();
+        }
+    }
+
+    private void adjustMouseSensibility()
+    {
+        if (zoomToggle)
+        {
+            fpsController.mouseLook.XSensitivity = zoomedInSensibility;
+            fpsController.mouseLook.YSensitivity = zoomedInSensibility;
+        }
+        else
+        {
+            fpsController.mouseLook.XSensitivity = zoomedOutSensibility;
+            fpsController.mouseLook.YSensitivity = zoomedOutSensibility;
+        }
+    }
+}
