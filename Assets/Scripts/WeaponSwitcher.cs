e863bdaaaffc65eb425d7c1cf1d9fefd2be76000
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-11-25 14:41:48 -0600

    Allow the player to wwitch weapons - initial setup
    Allow the player to wwitch weapons - initial setup

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] int currentWeapon = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetWeaponActive();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetWeaponActive()
+    {
+        int index = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (index == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            index++;
+        }
+    }
+}
