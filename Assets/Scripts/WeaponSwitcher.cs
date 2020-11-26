9d4e0945360b98fb9641cefd882bc9cc6e632f29
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-11-25 18:58:23 -0600

    Create Level 1 with music and enemies
    Create Level 1 with music and enemies

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -17,7 +17,59 @@ public class WeaponSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWhell();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
+
+    private void ProcessScrollWhell()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+
     }
 
     private void SetWeaponActive()
